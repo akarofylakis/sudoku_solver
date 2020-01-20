9fda24ab69c4895794f35e2cff6fa600e5c03a32
akarofylakis <akarofylakis@gmail.com>
2020-01-20 16:51:35 +0200

    Script complete

--- solver.py
+++ solver.py
@@ -42,8 +42,66 @@ def find_empty(b):
     :rtype: Tuple(int, int)
     """
     for i in range(len(b)):
-        for j in range(len(b[0]))
+        for j in range(len(b[0])):
             if b[i][j] == 0:
                 return (j, i)
 
+    return None
 
+def valid(b, num, pos):
+    """
+    Checks whether given solution (num) for cell (pos) is valid or not.  
+
+    :type b: List[int]
+    :type num: int
+    :type pos: Tuple(int, int)
+    :rtype: Boolean
+    """
+    # Check if row is valid
+    for i in range(len(b[0])):
+        if pos[1] != i and b[pos[0]][i] == num:
+            return False
+
+    # Check if column is valid
+    for j in range(len(b)):
+        if pos[0] != j and b[j][pos[1]] == num:
+            return False
+
+    # Check if box is valid
+    box_x = pos[0] // 3
+    box_y = pos[1] // 3
+    for j in range(box_y*3, box_y*3+3):
+        for i in range(box_x*3, box_x*3+3):
+            if b[i][j] == num and pos != (j, i):
+                return False
+    return True
+
+
+def solve(b):
+    """
+    Solves the board recursively using backtracking 
+
+    :type b: List[int]
+    :rtype: Boolean
+    """
+    find = find_empty(b)
+    if not find:
+        return True
+    else:
+        col, row = find
+
+    for i in range(1,10):
+        if valid(b, i, (row, col)):
+            b[row][col] = i
+
+            if solve(b):
+                return True
+
+            b[row][col] = 0
+
+    return False
+
+print_board(board)
+solve(board)
+print('')
+print_board(board)
